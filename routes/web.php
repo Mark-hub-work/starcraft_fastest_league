dd036b4c5cc3d6b82bca32958b59584ac688a980
Tangimark <mark@teamtangible.com>
2024-09-12 00:02:40 -0400

    Add some initial views and routes.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/rankings', function () {
+    return view('rankings');
+});
+
+
+Route::get('/rules', function () {
+    return view('rules');
+});
+
+Route::get('/streams', function () {
+    return view('streams');
 });
